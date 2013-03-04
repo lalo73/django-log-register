fdb64785a79149d07fc656a045b6e40d8be20680
lalo73 <leandro.gomez73@yahoo.com.ar>
2013-03-03 22:54:11 -0300

    Remoded more pycharm files and innecesary comments

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-log_register',
     version='0.1',
-    packages=['log_register'],
+    packages=['log-register'],
     include_package_data=True,
-    license='BSD License', # example license
+    license='BSD License',
     description='A simple Django app to register log ans save them.',
     long_description=README,
     url='https://github.com/lalo73/register-log',
@@ -21,7 +21,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
